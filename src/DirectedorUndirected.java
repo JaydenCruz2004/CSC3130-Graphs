987ed0e8340920a53b50c915c5b4b2b8340a38f3
Jayden <jaydencruz824@gmail.com>
2025-04-23 16:22:47 -0500

    fixed DirectedorUndirected.java

--- src/DirectedorUndirected.java
+++ src/DirectedorUndirected.java
@@ -1,24 +1,56 @@
+import java.util.Scanner;
+
 public class DirectedorUndirected {
 
-    public static void checkGraphType ( int[][] matrix){
-        int n = matrix.length;
-        boolean isDirected = false;
-
+    public static boolean isDirectedGraph(int[][] matrix, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] != matrix[j][i]) {
-                    isDirected = true;
-                    break;
+                    return true;
                 }
             }
-            if (isDirected) break;
         }
-
-        if (isDirected) {
-            System.out.println("The graph is directed.\n");
-        } else {
-            System.out.println("The graph is undirected.\n");
-        }
+        return false;
     }
-}
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the matrix (n x n): ");
+        int n = scanner.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        if (isDirectedGraph(matrix, n)) {
+            System.out.println("The given matrix represents a directed graph.");
+        } else {
+            System.out.println("The given matrix does not represent a directed graph.");
+        }
+
+        scanner.close();
+    }
+    //Example Input
+     /*
+    Undirected:
+    4
+    0101
+    1011
+    0100
+    1100
+     */
+     /*
+    Directed:
+    4
+    0100
+    0011
+    0000
+    1000
+     */
+}
